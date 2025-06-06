338605c0c729828751e95273a33f7cceaedb6ca3
Dan <101811313+D4NGK4@users.noreply.github.com>
2025-06-06 18:31:09 +0800

    Remove print statements and add Game class structure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
+import pygame, sys
+from settings import *
 
-print("Hello World!")
-print("Hello World Again!")
-print("I like typing this.")
-print("This is fun.")
-print("Yay! Printing.")
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
+class Game: 
+    def __init__(self) -> None:
+        pass 
